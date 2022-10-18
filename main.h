f52b5dd833a05832a49ba1c95251c931219eb1b6
chibwesamuel <102081659+chibwesamuel@users.noreply.github.com>
2022-10-18 13:38:50 +0200

    main.h update

--- main.h
+++ main.h
@@ -1,87 +1,55 @@
-#ifndef MAIN.H
-#define MAIN.H
+main.h.txt
+Who has access
+A
+System properties
+Type
+Text
+Size
+654 bytes
+Storage used
+654 bytes
+Location
+print_f
+Owner
+Antony Bahati
+Modified
+Oct 17, 2022 by Antony Bahati
+Opened
+1:38 PM by me
+Created
+Oct 17, 2022
+No description
+Viewers can download
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
-
 /**
-  * struct specifiers - The Struct
-  * @specifier: The specifier of the conversion
-  * @f: pointer to function
-  */
-typedef struct specifiers
-{
-	char *specifier;
-	int (*f)(va_list args);
-} spc_dt;
-
-/**
- * struct buffer_s - Defining a new buffer struct.
- * @buffer: Pointer to a character array.
- * @start: Pointer to the beginning of a buffer.
- * @len: string length as stored in buffer.
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-
-typedef struct buffer_s
+typedef struct print
 {
-	char *buffer;
-	char *start;
-	unsigned int len;
-} buffer_t;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-/*We need to handle conversion specifiers for c, s, %, d, i, etc*/
-unsigned int convert_c(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_s(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_di(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_percent(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_b(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_u(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_o(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_S(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_p(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_r(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_R(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-
-/* Handlers */
-unsigned char handle_flags(const char *flags, char *index);
-unsigned char handle_length(const char *modifier, char *index);
-int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
-
-/* Modifiers */
-unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-
-int _write(char c);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_a_char(va_list args);
-int _print_a_string(va_list args);
-int _print_format(const char *format, va_list args);
-int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
-int _print_a_integer(va_list args);
-void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
-int _validate_char(char _type);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif
+#endif  /* _MAIN_H */
